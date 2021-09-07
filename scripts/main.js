543c8b64d525c06dcbe38f27f9cd5810f70fa27d
theripper93 <tsplab@gmail.com>
2021-09-07 05:17:01 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,297 @@
+local = {
+    "en-US": {
+        modules:{
+            "Levels": {
+                name: "Levels",
+                icon: "fas fa-layer-group",
+                desc: "Create maps with multiple vertical levels on the same scene with seamless exploration and combat. Included 3D sight and more.",
+                status: "free",
+                media: "https://youtu.be/ELlweNunn4g",
+                link: "https://github.com/theripper93/Levels",
+              },
+              "Animated Walls": {
+                name: "Animated Walls",
+                icon: "fas fa-university",
+                desc: "Animate your walls with rotation and movement animations. Chain together animations with macros and hook macros on door opening.",
+                status: "free",
+                media: "https://streamable.com/wj3ecv",
+                link: "https://github.com/theripper93/animated-walls",
+              },
+              "Argon Combat HUD": {
+                name: "Argon Combat HUD",
+                icon: "fas fa-shield-alt",
+                desc: "Show a Solasta inspired combat hud for easy accessibility of actions\bonus actions etc.. Repelaces a lot of the character sheet functionality during combat so that you don't need to open it. Basically, token action HUD on steroids.",
+                status: "free",
+                media: "https://streamable.com/ujfxfu",
+                link: "https://github.com/theripper93/enhancedcombathud",
+              },
+              "Automated Evocations": {
+                name: "Automated Evocations",
+                icon: "fas fa-magic",
+                desc: "A user interface to manage companions with summoning animations and automated summoning for spells.",
+                status: "free",
+                media:
+                  "https://www.reddit.com/r/FoundryVTT/comments/pax0zn/automated_evocations_companion_manager/",
+                link: "https://github.com/theripper93/automated-evocations",
+              },
+              "Better Roofs": {
+                name: "Better Roofs",
+                icon: "fas fa-building",
+                desc: "Improve overhead tiles by adding funtionality to show them through the fog of war and even masking them based on vision to allow 'peeking' into windows/doors.",
+                status: "free",
+                media: "https://youtu.be/ELlweNunn4g",
+                link: "https://github.com/theripper93/Better-Roofs",
+              },
+              "Boss Bar": {
+                name: "Boss Bar",
+                icon: "fas fa-skull-crossbones",
+                desc: "Create a Dark Souls Like boss healthbar on top of the screen or in other locations. Customize texture, color and more.",
+                status: "free",
+                media:
+                  "https://www.reddit.com/r/FoundryVTT/comments/oirzdz/boss_bar_add_soulslike_boss_healthbars_to_your/",
+                link: "https://github.com/theripper93/Boss-Bar",
+              },
+              "Changelogs": {
+                name: "Changelogs",
+                icon: "fas fa-book-open",
+                desc: "Allow devs who opt into the system to display important messages to users after breaking updates, the user can chose which level of warning they wish to recive.",
+                status: "free",
+                media: "",
+                link: "https://github.com/theripper93/libChangelogs",
+              },
+              "Combat Booster": {
+                name: "Combat Booster",
+                icon: "fas fa-bolt",
+                desc: "Speed up your combat with Recent actions, Turn Marker and other smaller tweaks like Pan to combatant, body pile, mark defeated and more. (note that the recent action feature is only dnd5e compatible)",
+                status: "free",
+                media:
+                  "https://www.reddit.com/r/FoundryVTT/comments/ot0ek0/combat_booster_speed_up_your_combat/",
+                link: "https://github.com/theripper93/Combat-Booster",
+              },
+              "Dig Down": {
+                name: "Dig Down - Advanced Search",
+                icon: "fas fa-dungeon",
+                desc: "Search inside folders in the file picker, use special search terms to filter your sidebars and many other advanced search filters. Use the excavate functionality to automatically find images for tokens.",
+                status: "free",
+                media: "",
+                link: "https://github.com/theripper93/fuzzy-foundry",
+              },
+              "Go To Player": {
+                name: "Go To Player",
+                icon: "fas fa-arrow-alt-circle-up",
+                desc: "A simple module to get the GM to a specific player (go to it's viewed scene and pan the camera to their token). Simply click the new button in the player list context menu!",
+                status: "free",
+                media: "",
+                link: "https://github.com/theripper93/go-to-player",
+              },
+              "Hover Distance": {
+                name: "Hover Distance",
+                icon: "fas fa-arrows-alt-h",
+                desc: "Show distance to a token on hover without the need to use the ruler. Optionally supports inclusion of vertical distance",
+                status: "paid",
+                media: "https://streamable.com/2z5ijl",
+                link: "",
+              },
+              "Hurry Up": {
+                name: "Hurry Up",
+                icon: "fas fa-hourglass-half",
+                desc: "A simple timer to hurry up your players in combat! Tired of those pesky players taking forever to then just swing a longsword after a 10 minute wait? Well, i have the solution for you!",
+                status: "free",
+                media: "",
+                link: "https://github.com/theripper93/hurry-up",
+              },
+              "IP Shield": {
+                name: "IP Shield",
+                icon: "fas fa-shield-alt",
+                desc: "Simple module to hide the ip fields in the invite links dialog.",
+                status: "free",
+                media: "",
+                link: "https://github.com/theripper93/ip-protect",
+              },
+              "Levels - Automatic Cover Calculator": {
+                name: "Levels - Automatic Cover Calculator",
+                icon: "fas fa-crosshairs",
+                desc: "Calculates a precise cover % that can be used on any system do display a custom message above a token (eg, if only 30% visible display the obscured text). Include options to consider defeated enemies as cover but at a reduced height and more. Preconfigured for dnd5e but works with all systems (Active Effects dnd5e only). Also adds the ability to duck!",
+                status: "paid",
+                media: "https://streamable.com/0hr591",
+                link: "",
+              },
+              "Levels - Volumetric Templates": {
+                name: "Levels - Volumetric Templates",
+                icon: "fas fa-ruler-combined",
+                desc: "Compute Templates in 3D with autotargeting. Automatic mode to dynamically switch between 2d and 3d depending on the information provided. Works with all systems, MIDI QOL compatible.",
+                status: "paid",
+                media: "https://streamable.com/xxt7qf",
+                link: "",
+              },
+              "Patrol": {
+                name: "Patrol",
+                icon: "fas fa-walking",
+                desc: "Automatic or Fixed Patrol Routes for NPCs with the ability to spot player characters. Have your npc wander around to make your city map breath life, have guards on patrol, this module has you covered! (Works very well in conjunction with Auto-Rotate).",
+                status: "free",
+                media:
+                  "https://www.reddit.com/r/FoundryVTT/comments/noai9v/patrol_update_the_chase_is_on_free/",
+                link: "https://github.com/theripper93/Patrol",
+              },
+              "RPG Damage Numbers": {
+                name: "RPG Damage Numbers",
+                icon: "fas fa-dice-d20",
+                desc: "Show cascading damage numbers in the style of classic RPGs (for modern reference imagine Borderlands damage). Compatible only with dnd5e, requires midi-qol",
+                status: "paid",
+                media:
+                  "https://www.reddit.com/r/FoundryVTT/comments/p1c2jq/rpg_damage_numbers_add_some_style_to_your_combat/",
+                link: "",
+              },
+              "Smart Target": {
+                name: "Smart Target",
+                icon: "fas fa-bullseye",
+                desc: "Target with Alt+Click, For players, target by just left clicking a non-owned token, Show portraits instead of colored pips to indicate targets, positioning/offset and size of the icons can be configured in the module settings, Customize color/shape of the targeting reticule.",
+                status: "free",
+                media: "",
+                link: "https://github.com/theripper93/Smart-Target",
+              },
+              "Splatter": {
+                name: "Splatter",
+                icon: "fas fa-tint",
+                desc: "Add blood and gore to your games, Give that extra touch to your game. Featuring splats, bloodtrails, custom colors and more. Compatible with most game systems (minor configuration required).",
+                status: "free",
+                media:
+                  "https://www.reddit.com/r/FoundryVTT/comments/onpf3k/splatter_blood_up_your_games_free_based_on/",
+                link: "https://github.com/theripper93/Splatter",
+              },
+              "Stochastic, Fantastic! - Random Encounter Generator": {
+                name: "Stochastic, Fantastic! - Random Encounter Generator",
+                icon: "fas fa-dice-d20",
+                desc: "A random encounter generator with automatic creature importing (from your own compendiums) and spwaning plus loot generator (with automatic LootSheet creation). dnd5e only.",
+                status: "free",
+                media:
+                  "https://www.reddit.com/r/FoundryVTT/comments/p6ydy8/stochastic_fantastic_dnd5e_random_encounter/",
+                link: "https://github.com/theripper93/dnd-randomizer",
+              },
+              "Tile Sort": {
+                name: "Tile Sort",
+                icon: "fas fa-sort-alpha-down",
+                desc: "An easy to use tool to sort the z-index of tiles, includes also a toggle to hide one or more tiles (so you can work on tiles under them) and highlight to tiles on hover (so you can see a tile if it got lost under something).",
+                status: "paid",
+                media: "https://streamable.com/6f9ied",
+                link: "",
+              },
+              "Token Z": {
+                name: "Token Z",
+                icon: "fas fa-sort-amount-up",
+                desc: "Easly manage the Z-Index of tokens. The automatic sorting will keep larger tokens on the back and smaller on the front.",
+                status: "free",
+                media: "",
+                link: "",
+              },
+              "Weather blocker": {
+                name: "Weather blocker",
+                icon: "fas fa-cloud-sun",
+                desc: "A simple module to block weather effects on a map using drawings. Includes Better Roofs/Levels integration. Works with all systems.",
+                status: "free",
+                media:
+                  "https://www.reddit.com/r/FoundryVTT/comments/npixnk/free_weather_blocker_block_weather_through/",
+                link: "https://github.com/theripper93/Weather-Blocker",
+              },
+              "Taskbar": {
+                name: "Taskbar",
+                icon: "fas fa-tasks",
+                desc: "A fully functional Windows style taskbar for foundry, manage your stuff, the Easy way! Include Start style menu and utility icons.",
+                status: "paid",
+                media:
+                  "https://www.reddit.com/r/FoundryVTT/comments/pi0d9i/taskbar_a_windows_style_taskbar_for_foundry/",
+                link: "",
+              },
+              "Quickdraw": {
+                name: "Quickdraw",
+                icon: "fas fa-pencil-alt",
+                desc: "A rapid drawing tool to make quick drawings in-game by holding a hotkey (default q) with live preview for all clients. No more going to the drawing tools, and then deleting stuff! After a set timeout they will automatically fade out.",
+                status: "paid",
+                media: "https://streamable.com/gq5rkn",
+                link: "",
+              },
+        },
+        free : "Free",
+        paid : "Patreon Only",
+    },
+
+};
+const currentLocal = local[navigator.language] ? local[navigator.language] : local["en-US"];
+const modules = currentLocal.modules;
+
+let filter = 0;
+
+const filterColors = [
+  "rgb(255, 255, 255)",
+  "rgb(255, 98, 71)",
+  "rgb(128, 255, 185)",
+];
+
+const modlist = $(`.mod-list`);
+let sortedMods = [];
+let paidMods = [];
+let freeMods = [];
+for (let [k, v] of Object.entries(modules)) {
+  if (v.status == "paid") {
+    paidMods.push(k);
+  } else {
+    freeMods.push(k);
+  }
+}
+
+sortedMods.push(...paidMods.sort(), ...freeMods.sort());
+
+for (let mod of sortedMods) {
+  modlist.append(generateLi(modules[mod]));
+}
+
+function generateLi(data) {
+  const $li = $(`<li class="module-item" data-status="${data.status}">
+    <i class="${data.icon} mod-icon"></i>
+    <div class="module-info">
+    <a class"module-title-link" href="${
+      data.link
+    }" target="_blank" rel="noopener noreferrer"><h3>${data.name}</h3></a>
+    <p>${data.desc}</p>
+    <div class="info-links">
+        <span>Status: <a href="https://www.patreon.com/theripper93" target="_blank" rel="noopener noreferrer" class="${
+          data.status
+        }">${data.status == "free" ? currentLocal.free : currentLocal.paid}</a></span>
+        <a href="${
+          data.media
+        }" target="_blank" rel="noopener noreferrer"><i class="fas fa-play"></i></a>
+    </div>
+    </div>
+    </li>`);
+  return $li;
+}
+
+$(document).on("click", ".filter", function (e) {
+  if (filter == 2) filter = 0;
+  else filter++;
+
+  $(e.currentTarget).css({
+    color: filterColors[filter],
+  });
+  modlist.find(".module-item").each(function (i, el) {
+    switch (filter) {
+      case 0:
+        $(el).slideDown(200);
+        break;
+      case 1:
+        if ($(el).data("status") == "paid") {
+          $(el).slideDown(200);
+        } else {
+          $(el).slideUp(200);
+        }
+        break;
+      case 2:
+        if ($(el).data("status") == "free") {
+          $(el).slideDown(200);
+        } else {
+          $(el).slideUp(200);
+        }
+        break;
+    }
+  });
+});
