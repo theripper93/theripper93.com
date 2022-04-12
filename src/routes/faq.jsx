1b6f6dbe5e66b286aa45cfeab54a96fe39c9a06f
April Tao <95392008+CasanovaSekova@users.noreply.github.com>
2022-04-12 14:30:32 +0200

    CSS and Navbar

---
+++ src/routes/faq.jsx
@@ -0,0 +1,25 @@
+import { Outlet } from 'react-router';
+import App from '../App';
+import Navbar from '../components/Navbar';
+import { useLocation } from "react-router-dom";
+
+export default function Info() {
+
+  let location = useLocation();
+
+  const faqCards = () => {
+    return (
+      <section className="info">
+      <h1>Hello World</h1>
+    </section>
+    )
+  };
+
+  return (
+    <div className='wrapper'>
+      <Navbar></Navbar>
+      <Outlet />
+      {location?.endsWith('faq') && faqCards()}
+    </div>
+  )
+}
