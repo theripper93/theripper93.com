2916635156309fa3646b849c5c8e3421fcd8b8a7
April Tao <95392008+apriltaoyvr@users.noreply.github.com>
2022-05-22 19:00:42 -0700

    Styling fixes

--- src/routes/moduleSubpage.jsx
+++ src/routes/moduleSubpage.jsx
@@ -13,15 +13,13 @@ import Navbar from '../components/navbar/Navbar';
 import FaqCard from '../components/subpage/FaqCard';
 import ModuleGuide from '../components/subpage/ModuleGuide.jsx';
 
-
 const Subpage = () => {
-
   const [premiumData, setPremiumData] = useState(null);
   const [data, setData] = useState({});
 
   const moduleId = window.location.hash.split('/').pop();
 
-  locData().then(d => setData(d.modules.find(mod => mod.id === moduleId)))
+  locData().then((d) => setData(d.modules.find((mod) => mod.id === moduleId)));
   premiumMods().then((pData) => {
     if (pData[moduleId]) {
       setPremiumData(pData[moduleId]);
@@ -34,7 +32,7 @@ const Subpage = () => {
   return (
     <div className={`subpage ${data.status}-wrapper`}>
       <Navbar></Navbar>
-      <section className={`module-intro${!data.media ? " no-video" : ""}`}>
+      <section className={`module-intro${!data.media ? ' no-video' : ''}`}>
         <main>
           <article className='card-solid'>
             <div className='content-wrapper'>
@@ -42,36 +40,70 @@ const Subpage = () => {
                 <h1>{data.name}</h1>
               </header>
               <p>{data.fulldesc}</p>
-              <a href={premiumData?.downloadURL ?? data.link} rel="noreferrer" target="_blank"><button className={`${data.status}-background`}>{premiumData?.downloadURL ? "Download" : "GitHub"}</button></a>
+              <a
+                href={premiumData?.downloadURL ?? data.link}
+                rel='noreferrer'
+                target='_blank'
+              >
+                <button className={`${data.status}-background`}>
+                  {premiumData?.downloadURL ? 'Download' : 'GitHub'}
+                </button>
+              </a>
             </div>
-            <footer className="badge">
-              <div className={data.status}>
-                {data.statusText}
-              </div>
+            <footer className='badge'>
+              <div className={data.status}>{data.statusText}</div>
             </footer>
           </article>
-          <aside>
-            {data.media && <Video module={data}></Video>}
-          </aside>
+          <aside>{data.media && <Video module={data}></Video>}</aside>
         </main>
-        {(troubleshooting.length > 0 || guides.length > 0) && <FontAwesomeIcon onClick={()=>  document.getElementById("info").scrollIntoView({ behavior: 'smooth', block: 'start' })} icon="fa-solid fa-arrow-down" size="5x" fixedWidth />}
+        {(troubleshooting.length > 0 || guides.length > 0) && (
+          <FontAwesomeIcon
+            onClick={() =>
+              document
+                .getElementById('info')
+                .scrollIntoView({ behavior: 'smooth', block: 'start' })
+            }
+            icon='fa-solid fa-arrow-down'
+            size='5x'
+            fixedWidth
+          />
+        )}
       </section>
-      <section className='additional content' id="info">
-        {troubleshooting.length > 0 && <section className='module-troubleshooting'>
-          <h2>Troubleshooting</h2>
-          <main>
-            {troubleshooting.map((troub, index) => <FaqCard key={index} faq={{ question: troub.title, answer: troub.desc, status: "basic" }}></FaqCard>)}
-          </main>
-        </section>}
-        {guides.length > 0 && <section className='module-guides'>
-          <h2>Guides</h2>
-          <main>
-            {guides.map((guide, index) => <ModuleGuide key={index} title={guide.title} url={guide.url}></ModuleGuide>)}
-          </main>
-        </section>}
+      <section className='additional content' id='info'>
+        {troubleshooting.length > 0 && (
+          <section className='module-troubleshooting'>
+            <h2>Troubleshooting</h2>
+            <main>
+              {troubleshooting.map((troub, index) => (
+                <FaqCard
+                  key={index}
+                  faq={{
+                    question: troub.title,
+                    answer: troub.desc,
+                    status: 'basic',
+                  }}
+                ></FaqCard>
+              ))}
+            </main>
+          </section>
+        )}
+        {guides.length > 0 && (
+          <section className='module-guides'>
+            <h2>Guides</h2>
+            <main>
+              {guides.map((guide, index) => (
+                <ModuleGuide
+                  key={index}
+                  title={guide.title}
+                  url={guide.url}
+                ></ModuleGuide>
+              ))}
+            </main>
+          </section>
+        )}
       </section>
     </div>
-  )
-}
+  );
+};
 
 export default Subpage;
